4c3f945c6c89b23b74d797c850a74a823066d882
bartosz121 <bmagiera121@gmail.com>
2022-04-18 13:49:13 +0200

    Init flask app

---
+++ news_scraper_api/app.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, Flask
+from flask_restful import Api
+
+from config import get_config
+from core.db import mongo
+from resources.news import News
+
+
+def create_app(config_obj):
+    app = Flask(__name__)
+    app.config.from_object(config_obj)
+    mongo.init_app(app)
+
+    api_bp = Blueprint("api", __name__)
+    api = Api(api_bp)
+
+    api.add_resource(News, "/api/v1/news", "/api/v1/news/<string:id>")
+
+    app.register_blueprint(api_bp)
+
+    return app
+
+
+if __name__ == "__main__":
+    config_obj = get_config()
+    app = create_app(config_obj)
+    app.run()
---
+++ news_scraper_api/config.py
@@ -0,0 +1,27 @@
+import os
+from typing import Type
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_config() -> Type["Config"]:
+    config_obj = DevConfig
+    if bool(os.getenv("PROD")):
+        config_obj = ProdConfig
+
+    return config_obj
+
+
+class Config:
+    pass
+
+
+class ProdConfig(Config):
+    DEBUG = False
+    MONGODB_SETTINGS = {"host": os.getenv("PROD_MONGO_URI")}
+
+
+class DevConfig(Config):
+    DEBUG = True
+    MONGODB_SETTINGS = {"host": os.getenv("DEV_MONGO_URI")}
